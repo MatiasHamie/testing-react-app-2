fe58ffef163906642538ff4c217674bc8665fd6f
MatiasHamie <mnhamie@gmail.com>
2021-05-16 15:16:41 -0300

    Pruebas basicas sobre formulario (input OnChange y form OnSubmit)

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export const AddCategory = ({ setCategories }) => {
-  const [inputValue, setInputValue] = useState(""); // ''
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
+    console.log("valor recibido", e.target.value);
     setInputValue(e.target.value);
+    console.log("inputValue", inputValue);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (inputValue.trim().length > 2) {
       setCategories((cats) => [inputValue, ...cats]);
       setInputValue("");
@@ -18,9 +19,12 @@ export const AddCategory = ({ setCategories }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-    </form>
+    <>
+      <p>{inputValue}</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </form>
+    </>
   );
 };
 
